be072ee8f9bd5ca8b3f41979252776c4334550c6
jbackman <github@jitonline.net>
2014-12-31 16:57:01 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ CURPATH=$(pwd)
 tar -xf libdnet-1.11.tar.gz
 tar -xf dnstracer-1.9.tar.gz
 tar -xf ftpgrab_0.1.5.orig.tar.gz
+tar -xf unicornscan-0.4.7-2.tar.bz2
 cd ${CURPATH}/libdnet-1.11
 ./configure && make && make install
 cd ${CURPATH}/dnstracer-1.9
